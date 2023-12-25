7564ebf344441477c110f37d8bbff421d35252db
zhrwmmdi <zahra.mmdi2003@gmail.com>
2023-12-25 21:38:14 +0330

    DataHandler class added for readind data from txt file and converting them to a matrix, thh program starts from this class initialization in main method

---
+++ src/main/java/DataHandler.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class DataHandler {
+    private final Scanner reader;
+    private int counter;
+    File numbersFile;
+
+    //Matrix matrix;
+    public DataHandler(String filePath) throws FileNotFoundException {
+        numbersFile = new File(filePath);
+        reader = new Scanner(numbersFile);
+            while (reader.hasNext()){
+                counter++;
+                reader.nextInt();
+            }
+    }
+
+    public double[][] produceMatrix(){
+        double[][] matrix = new double[(int) Math.sqrt(counter)][(int) Math.sqrt(counter)];
+        readAndStoreNumbers(matrix);
+        return matrix;
+    }
+    private void readAndStoreNumbers(double[][] matrix)   {
+
+        try (Scanner reader = new Scanner(numbersFile)){
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix.length; j++) {
+                    if (reader.hasNext()){
+                        matrix[i][j] = reader.nextInt();
+                    }
+                }
+            }
+        }catch (Exception ex){
+            System.out.println("SOMETHING WENT WRONG IN DataHandler.readAndStoreNumbers() method.\n"+ex.getMessage());
+        }
+    }
+    public void printMatrix(double[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        DataHandler input = new DataHandler("matrix.txt");
+        double[][] matrix = input.produceMatrix();
+
+        double determinant = Gauss.gauss(matrix, matrix.length);
+        System.out.println(determinant);
+        
+    }
+}
